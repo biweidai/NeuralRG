d7ebd952036763bfdbb5e9681fb77e3f602c7ec8
biwei <biwei@berkeley.edu>
2023-01-03 15:00:36 -0500

    add ising_x in __init__

--- source/__init__.py
+++ source/__init__.py
@@ -1,6 +1,6 @@
 from .gaussian import Gaussian
 from .phi4complex import Phi4
 from .ringLike import Ring2d
-from .ising import Ising
+from .ising import Ising, Ising_x
 from .source import Source
 from .uniform import Uniform
